090014f75d10ccd962a3dcc45019cffd5c2f9bfb
taotao0501 <21707001sbt@zju.edu.cn>
2021-12-11 23:05:48 +0800

    BST前序遍历

---
+++ src/Trees/BST/Main.java
@@ -0,0 +1,24 @@
+package Trees.BST;
+
+/**
+ * @Description:
+ * @Author: Bentao She
+ * @Date: 2021/12/11 22:37
+ * @Version: V1.0
+ **/
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5,3,6,8,4,2};
+        for(int num : nums) {
+            bst.add(num);
+        }
+        bst.preOrder();
+        System.out.println();
+
+        System.out.println(bst);
+    }
+}
