2139af7b099b1ce924c2a0709eb7f71fd016e5cb
taotao0501 <21707001sbt@zju.edu.cn>
2022-04-24 20:48:28 +0800

    18题

--- src/LeetCodeCompletePlan/Offer/ListNode.java
+++ src/LeetCodeCompletePlan/Offer/ListNode.java
@@ -15,4 +15,9 @@ public class ListNode {
     ListNode(int x) {
         val = x;
     }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
